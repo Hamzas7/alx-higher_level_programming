80919f095e4eed2fa575ad786c01293fa34a8e41
hamzas7 <hamza.yasr7@gmail.com>
2024-04-26 14:54:17 -0700

    Time for an interview!

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,10 +1,22 @@
 #!/usr/bin/python3
-"""fetches https://intranet.hbtn.io/status."""
 import requests
 
-
 if __name__ == "__main__":
-    r = requests.get("https://intranet.hbtn.io/status")
+    # Define the URL to fetch
+    url = 'https://alx-intranet.hbtn.io/status'
+
+    # Send a GET request to the URL using the requests package
+    response = requests.get(url)
+
+    # Parse the response body as JSON
+    body_dict = response.json()
+
+    # Print the response body in the required format
     print("Body response:")
-    print("\t- type: {}".format(type(r.text)))
-    print("\t- content: {}".format(r.text))
+
+    # Print the type of the 'status' field in the response body
+    print("\t- type:", type(body_dict['status']))
+
+    # Print the content of the 'status' field in the response body
+    print("\t- content:", body_dict['status'])
+
